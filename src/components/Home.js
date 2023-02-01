9fa57b4f9269639abd4c3f58d4532a39421494c5
nishadahmed5 <nishatahmed650@gmail.com>
2023-02-02 02:26:26 +0600

    login page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your authentication logic here.
+    console.log(`Username: ${email}, Password: ${password}`);
+  };
   return (
     <div>
-      <h2>This is home</h2>
+      <section className="min-h-screen flex justify-center items-center ">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Email:</label>
+            <input
+              className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <button
+            className="bg-blue-700 w-full mt-5 rounded text-white text-lg py-3"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </section>
     </div>
   );
 };
