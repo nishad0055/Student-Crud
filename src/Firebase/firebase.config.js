ddab5f1bf5e9ef88d9ff9fa18a899ea25c41b882
nishadahmed5 <nishatahmed650@gmail.com>
2023-02-02 02:06:49 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnA2ttIj0EKRlxgDveBt-IkxgatgSXH44",
+  authDomain: "user-management-caa78.firebaseapp.com",
+  projectId: "user-management-caa78",
+  storageBucket: "user-management-caa78.appspot.com",
+  messagingSenderId: "226950910606",
+  appId: "1:226950910606:web:bb65b9787f1f7467a3efdf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <section>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <p>User email</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Navbar;
